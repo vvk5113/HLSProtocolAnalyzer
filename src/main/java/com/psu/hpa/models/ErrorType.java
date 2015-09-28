c3e83a6f21154a1d1734bff119a3bf228af7c465
“vvk5113” <vvk5113@psu.edu>
2015-09-28 15:26:06 -0400

    New File

---
+++ src/main/java/com/psu/hpa/models/ErrorType.java
@@ -0,0 +1,29 @@
+package com.psu.hpa.models;
+
+public enum ErrorType {
+	
+	TYPE("Error Type"),
+
+	MISSING_TAG("Missing Tag"),
+
+	MISSING_STREAM_FILE("Missing Stream File"),
+	
+	MISSING_SEGMENT_FILE("Missing Segment File"),
+	
+	INCORRECT_FORMAT("Incorrect Format"),
+	
+	DUPLICATE_TAG("Duplicate Tag"),
+	
+	FUNCTIONAL_TAG("Functional");
+	
+	private String value;
+	
+	ErrorType(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+};
